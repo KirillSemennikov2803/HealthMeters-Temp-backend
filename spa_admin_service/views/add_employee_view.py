deb263303bd15e699ba0c6b60006f65733f7c58f
Kirill <semennikov1360@mail.ru>
2020-06-09 03:57:08 +0300

    bug fix

--- spa_admin_service/views/add_employee_view.py
+++ spa_admin_service/views/add_employee_view.py
@@ -1,13 +1,16 @@
+import datetime
+
 from rest_framework.views import APIView
 
-from general_module.models import AdminLicense, Company, User
+from general_module.models import AdminLicense, Company, User, License
+from main.guid_generater import generate_user_guid
 from main.response_processing import get_success_response, get_error_response
 from main.sessions_storage import validate_session, validate_license, get_user
 
 
 class UserView(APIView):
-    @validate_session
-    @validate_license
+    @validate_session()
+    @validate_license()
     def post(self, request):
         try:
             session = request.data["session"]
@@ -20,10 +23,20 @@ class UserView(APIView):
             company_name = get_user(session)
 
             company = Company.objects.filter(name=company_name)[0]
+            guid = generate_user_guid()
 
-            user = User.objects.create(full_name=full_name, telegram_nick=tg_nick, position=role, company=company)
+            now = datetime.datetime.utcnow()
+            license_bd = License.objects.filter(company=company, start_date__lte=now, end_date__gte=now)[0]
+
+            if license_bd.count_of_people >= company.active_people:
+                return get_success_response({"status": "error",
+                                             "reason": "licenceEmployeesBoundaryReached"})
+
+            user = User.objects.create(guid=guid, full_name=full_name, telegram_nick=tg_nick, position=role,
+                                       company=company)
 
             company.active_people += 1
             company.save()
+            return get_success_response({"status": "ok"})
         except:
             return get_error_response(500)
