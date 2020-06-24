e2ef752d42fabf4bba9d92314df509db36a83477
Konstantin Leladze <leladze.kg@phystech.edu>
2020-06-24 09:51:54 +0300

    changed /authoriSe to /authoriZe.

--- spa_admin_service/urls.py
+++ spa_admin_service/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from spa_admin_service.views import \
     company_view, get_employees_data_view, delete_employee_view, edit_employee_view, \
-    get_employees_view, authorise_view, register_view, licence_view, add_employee_view
+    get_employees_view, authorize_view, register_view, licence_view, add_employee_view
 
 app_name = "spa_admin_service"
 
@@ -13,7 +13,7 @@ urlpatterns = [
     path('delete_employee', delete_employee_view.UserView.as_view()),
     path('edit_employee', edit_employee_view.UserView.as_view()),
     path('get_employees', get_employees_view.UserView.as_view()),
-    path('authorize', authorise_view.UserView.as_view()),
+    path('authorize', authorize_view.UserView.as_view()),
     path('register', register_view.UserView.as_view()),
     path('licence', licence_view.UserView.as_view()),
     path('add_employee', add_employee_view.UserView.as_view()),
