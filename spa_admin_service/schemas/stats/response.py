57a64b7218a39a1f0654e5091a59c4f36c57a3eb
Konstantin Leladze <leladze.kg@phystech.edu>
2020-06-25 00:28:46 +0300

    fixed stats response schema

--- spa_admin_service/schemas/stats/response.py
+++ spa_admin_service/schemas/stats/response.py
@@ -1,7 +1,11 @@
 res_schema = {
     "type": "object",
     "properties": {
-        "session": {"type": "integer",
-                    "minimum": 0}
+        "employeesCount": {
+            "type": "integer",
+            "minimum": 0
+        }
     },
+    "required": ["employeesCount"],
+    "additionalProperties": False,
 }
