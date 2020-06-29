87e560ab0a80b18b2a375d33c1e840bb2a7207a0
KirillSemennikov <Semennikov1360@mail.ru>
2020-06-29 17:55:03 +0300

    add cors support

--- HealthMeters/settings.py
+++ HealthMeters/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'telegram_bot_service',
     'spa_admin_service',
     'general_module',
+    'corsheaders',
 ]
 
 CORS_ORIGIN_WHITELIST = [
@@ -95,6 +96,20 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+CORS_ORIGIN_WHITELIST = [
+    "http://127.0.0.1:1329",
+    "http://localhost:1329",
+    "http://209.97.183.71:1329",
+]
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE',
+    'OPTIONS',
+    'PATCH',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
