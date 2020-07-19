166e1eed42df18865324b5574f5a7c694cdc9d78
Kirill Semennikov <Semennikov1360@mail.ru>
2020-07-19 17:37:41 +0300

    add Docker

--- HealthMeters/settings.py
+++ HealthMeters/settings.py
@@ -13,16 +13,26 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import random
+import string
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd)0(e09n8fpvuy79aspf%&h4h#%wq7!63wk*2m*$60)a5)e^d1'
+SECRET_KEY = os.environ.get("SECRET_KEY", default=get_random_string(15))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "151.248.122.100", "0.0.0.0", "desktop-p9l3c31"]
 
