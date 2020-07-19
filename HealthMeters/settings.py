11ec5aee549297eca20cb18cc8d84aaafa0f4454
Kirill Semennikov <Semennikov1360@mail.ru>
2020-07-19 18:54:00 +0300

    upgrade Docker

--- HealthMeters/settings.py
+++ HealthMeters/settings.py
@@ -140,5 +140,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
-STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR
+STATIC_URL = BASE_DIR + '/static/'
