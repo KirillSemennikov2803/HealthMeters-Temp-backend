301f3fcbc36a404546e059d9db1061ea27ff1d38
Konstantin Leladze <leladze.kg@phystech.edu>
2020-07-29 05:02:22 +0300

    Fixed bug with GET request type.

--- main/request_proccessing.py
+++ main/request_proccessing.py
@@ -7,9 +7,9 @@ from requests import Response
 
 class RequestType(Enum):
     Post = "POST"
-    Get = "Get"
+    Get = "GET"
 
 
-def send_request(type_request: RequestType, url: str, headers: dict, payload: dict) -> Response:
-    response = requests.request(type_request.value, url, headers=headers, data=payload)
+def send_request(request_type: RequestType, url: str, headers: dict, payload: dict) -> Response:
+    response = requests.request(request_type.value, url, headers=headers, data=payload)
     return response
